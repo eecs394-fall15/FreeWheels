5878f897edde9abf2ce49d587ca48e030ed52421
jaffemd <jaffemd0125@gmail.com>
2015-11-10 20:49:27 -0600

    Added icons

--- dist/app/example.js
+++ dist/app/example.js
@@ -7,12 +7,12 @@ angular
   .module('example')
   .controller('DrawerController', function($scope, supersonic) {
     $scope.typesList = [
-                  {'name':'Animals','checked': true}, 
-                  {'name':'Library','checked': true},
-                  {'name':'Museums and Art','checked': true},
-                  {'name':'Nature','checked': true},
-                  {'name':'Things to do','checked': true},
-                  {'name':'Places of worship','checked': true}];
+                  {'name':'Animals','checked': true,'icon':'ios-paw-outline'}, 
+                  {'name':'Library','checked': true, 'icon':'ios-book-outline'},
+                  {'name':'Museums and Art','checked': true, 'icon':'ios-flask-outline'},
+                  {'name':'Nature','checked': true, 'icon':'leaf'},
+                  {'name':'Things to do','checked': true, 'icon':'ios-americanfootball-outline'},
+                  {'name':'Places of worship','checked': true, 'icon':'ios-moon-outline'}];
 
 
    $scope.submitFilters = function()
