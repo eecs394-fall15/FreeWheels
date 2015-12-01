b41470296abebc399ab580daa22d5a1b61013312
Anuj Iravane <anudge7@gmail.com>
2015-12-01 15:03:05 -0600

    remove unused controllers

--- app/example/scripts/NavigateController.js
+++
@@ -1,8 +0,0 @@
-angular
-  .module('example')
-  .controller('NavigateController', function($scope, supersonic) {
-    
-    supersonic.ui.views.current.whenVisible( function(){
-      $scope.name = steroids.view.params.id;
-    });
-  })
